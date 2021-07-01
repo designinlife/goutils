e5bd06d8af6c784ccbd94510dd29b5b40c25f182
李磊 <lilei@dongyou.com>
2021-07-01 11:53:05 +0800

    Update ssh_test.go

--- ssh_test.go
+++ ssh_test.go
@@ -5,6 +5,10 @@ import (
 )
 
 func TestSSHClient_Upload(t *testing.T) {
-	// client := NewSSHClient("127.0.0.1", 22, "root", "~/.ssh/id_rsa", false)
-	// client.Upload("d:/tmp/php-8.0.7.tar.gz", "/root/php-8.0.7.tar.gz")
+	client := NewSSHClient("127.0.0.1", 22, "root", "~/.ssh/id_rsa", false, SSHOptionWithProxy("http://127.0.0.1:3128"))
+	err := client.Upload("d:/tmp/php-8.0.7.tar.gz", "/root/php-8.0.7.tar.gz")
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
 }
