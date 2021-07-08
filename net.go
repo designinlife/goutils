820567234c7e0b2b3b3f18c368ebb27858d3abf6
李磊 <lilei@dongyou.com>
2021-07-08 16:52:07 +0800

    Update

--- net.go
+++ net.go
@@ -93,3 +93,21 @@ func init() {
 	proxy.RegisterDialerType("http", newHTTPProxy)
 	proxy.RegisterDialerType("https", newHTTPProxy)
 }
+
+// GetLocalIPAddr 读取本机 IP 地址。
+func GetLocalIPAddr() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("Failed to read the IP address of the machine.")
+}
