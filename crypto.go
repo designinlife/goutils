52a36d966d51e226940aa3252de1b1856ac8eace
李磊 <lilei@dongyou.com>
2021-07-08 15:04:47 +0800

    Add GenerateSelfSignedCertKey() func.

--- crypto.go
+++ crypto.go
@@ -3,11 +3,21 @@ package goutils
 import (
 	"crypto/hmac"
 	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"hash"
+	"math/big"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type HashAlgorithm string
@@ -125,4 +135,74 @@ func HMAC(str, key string, algorithm HashAlgorithm, capital bool) string {
 	}
 
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// GenerateSelfSignedCertKey 生成自签名证书。
+func GenerateSelfSignedCertKey(outdir string, keySize int, expire time.Duration, host string, alternateIPs []net.IP, alternateDNS []string) error {
+	// 1.生成密钥对
+	priv, err := rsa.GenerateKey(rand.Reader, keySize)
+
+	if err != nil {
+		return err
+	}
+
+	// 2.创建证书模板
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1), // 该号码表示CA颁发的唯一序列号，在此使用一个数来代表
+		Issuer:       pkix.Name{},
+		Subject: pkix.Name{
+			CommonName: fmt.Sprintf("%s@%d", host, time.Now().Unix()),
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().Add(expire),
+		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign, // 表示该证书是用来做服务端认证的
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	if ip := net.ParseIP(host); ip != nil {
+		template.IPAddresses = append(template.IPAddresses, ip)
+	} else {
+		template.DNSNames = append(template.DNSNames, host)
+	}
+
+	template.IPAddresses = append(template.IPAddresses, alternateIPs...)
+	template.DNSNames = append(template.DNSNames, alternateDNS...)
+
+	// 3.创建证书,这里第二个参数和第三个参数相同则表示该证书为自签证书，返回值为DER编码的证书
+	certificate, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return err
+	}
+
+	// 4.将得到的证书放入pem.Block结构体中
+	block := pem.Block{
+		Type:    "CERTIFICATE",
+		Headers: nil,
+		Bytes:   certificate,
+	}
+
+	// 5.通过pem编码并写入磁盘文件
+	file, err := os.Create(filepath.Join(outdir, "ca.crt"))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	pem.Encode(file, &block)
+
+	// 6.将私钥中的密钥对放入pem.Block结构体中
+	block = pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   x509.MarshalPKCS1PrivateKey(priv),
+	}
+
+	// 7.通过pem编码并写入磁盘文件
+	file, err = os.Create(filepath.Join(outdir, "ca.key"))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	pem.Encode(file, &block)
+
+	return nil
 }
