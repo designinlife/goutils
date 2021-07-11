a1a9b645d22bb63818e09b0a8906dac6875af1a9
李磊 <lilei@dongyou.com>
2021-07-11 12:28:35 +0800

    重构 HttpClient

--- http.go
+++ http.go
@@ -3,154 +3,179 @@ package goutils
 import (
 	"bytes"
 	"crypto/tls"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"github.com/dustin/go-humanize"
 	"github.com/pkg/errors"
-	logger "github.com/sirupsen/logrus"
-	"io"
 	"io/ioutil"
 	"net/http"
-	url2 "net/url"
-	"os"
-	"path/filepath"
-	"strconv"
+	"net/url"
 	"strings"
 	"time"
 )
 
-type WebClient struct {
-	Debug          bool
-	Proxy          string
-	Headers        []WebHeader
-	DefaultTimeout time.Duration
+type HttpClient struct {
 }
 
-type WebHeader struct {
-	Name  string
-	Value string
+type HttpRequest struct {
+	Timeout             time.Duration
+	Query               interface{}
+	Headers             map[string]interface{}
+	Cookies             interface{}
+	FormParams          map[string]interface{}
+	JSON                interface{}
+	XML                 interface{}
+	Proxy               string
+	AllowNon200Response bool
 }
 
-type WebClientOption func(*WebClient)
-
-func NewHTTPClient() *WebClient {
-	return &WebClient{}
+type HttpResponse struct {
+	StatusCode    int
+	RequestURI    string
+	Header        http.Header
+	ContentType   string
+	ContentLength int64
+	Body          []byte
 }
 
-func NewHTTPClientWithTimeout(timeout time.Duration) *WebClient {
-	return &WebClient{
-		DefaultTimeout: timeout,
+func NewHttpClient() *HttpClient {
+	return &HttpClient{}
+}
+
+func (h *HttpClient) Request(method, uri string, r *HttpRequest) (*HttpResponse, error) {
+	// 创建 HTTP 客户端实例
+	req, _ := http.NewRequest(method, uri, nil)
+
+	// 设置 Headers
+	if r.Headers != nil {
+		for k, v := range r.Headers {
+			if vv, ok := v.(string); ok {
+				req.Header.Set(k, vv)
+				continue
+			}
+			if vv, ok := v.([]string); ok {
+				for _, vvv := range vv {
+					req.Header.Add(k, vvv)
+				}
+			}
+		}
 	}
-}
 
-func NewHTTPClientWithOptions(opts ...WebClientOption) *WebClient {
-	wc := &WebClient{}
-
-	for _, opt := range opts {
-		opt(wc)
+	if req.Header.Get("User-Agent") == "" {
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 	}
 
-	return wc
-}
-
-func HTTPOptionWithProxy(proxyUrl string) WebClientOption {
-	return func(wc *WebClient) {
-		wc.Proxy = proxyUrl
-	}
-}
-
-func HTTPOptionWithTimeout(timeout time.Duration) WebClientOption {
-	return func(wc *WebClient) {
-		wc.DefaultTimeout = timeout
-	}
-}
-
-func HTTPOptionWithHeaders(headers map[string]string) WebClientOption {
-	return func(wc *WebClient) {
-		for k, v := range headers {
-			wc.Headers = append(wc.Headers, WebHeader{
+	// 设置 Cookies
+	switch r.Cookies.(type) {
+	case string:
+		cookies := r.Cookies.(string)
+		req.Header.Add("Cookie", cookies)
+	case map[string]string:
+		cookies := r.Cookies.(map[string]string)
+		for k, v := range cookies {
+			req.AddCookie(&http.Cookie{
 				Name:  k,
 				Value: v,
 			})
 		}
-	}
-}
-
-func (wc *WebClient) doRequest(method, url string, queryParams map[string]string, formParams url2.Values, jsonData []byte, timeout time.Duration, filename string, showProgressBar bool) ([]byte, error) {
-	if queryParams != nil {
-		q := url2.Values{}
-
-		for k, v := range queryParams {
-			q.Add(k, v)
-		}
-
-		if strings.Contains(url, "?") {
-			url = url + "&" + q.Encode()
-		} else {
-			url = url + "?" + q.Encode()
+	case []*http.Cookie:
+		cookies := r.Cookies.([]*http.Cookie)
+		for _, cookie := range cookies {
+			req.AddCookie(cookie)
 		}
 	}
 
-	request, _ := http.NewRequest(method, url, nil)
+	// 设置 Query 查询参数
+	switch r.Query.(type) {
+	case string:
+		str := r.Query.(string)
+		req.URL.RawQuery = str
+	case map[string]interface{}:
+		q := req.URL.Query()
+		for k, v := range r.Query.(map[string]interface{}) {
+			if vv, ok := v.(string); ok {
+				q.Set(k, vv)
+				continue
+			}
+			if vv, ok := v.([]string); ok {
+				for _, vvv := range vv {
+					q.Add(k, vvv)
+				}
+			}
+		}
+		req.URL.RawQuery = q.Encode()
+	}
 
-	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+	// 设置 Form 表单参数
+	if r.FormParams != nil {
+		if _, ok := r.Headers["Content-Type"]; !ok {
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		}
 
-	if wc.Headers != nil {
-		for i := 0; i < len(wc.Headers); i++ {
-			request.Header.Set(wc.Headers[i].Name, wc.Headers[i].Value)
+		values := url.Values{}
+		for k, v := range r.FormParams {
+			if vv, ok := v.(string); ok {
+				values.Set(k, vv)
+			}
+			if vv, ok := v.([]string); ok {
+				for _, vvv := range vv {
+					values.Add(k, vvv)
+				}
+			}
+		}
+		req.Body = ioutil.NopCloser(strings.NewReader(values.Encode()))
+	}
+
+	// 设置 JSON 请求
+	if r.JSON != nil {
+		if _, ok := r.Headers["Content-Type"]; !ok {
+			req.Header.Set("Content-Type", "application/json")
+		}
+
+		b, err := json.Marshal(r.JSON)
+		if err == nil {
+			req.Body = ioutil.NopCloser(bytes.NewReader(b))
 		}
 	}
 
-	if timeout <= 0 {
-		timeout = wc.DefaultTimeout
-	}
-	if timeout <= 0 {
-		timeout = time.Second * 60
-	}
-
-	var client *http.Client
-
-	if wc.Proxy != "" {
-		proxy, _ := url2.Parse(wc.Proxy)
-
-		tr := &http.Transport{
-			Proxy: http.ProxyURL(proxy),
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
+	// 设置 XML 请求
+	if r.XML != nil {
+		if _, ok := r.Headers["Content-Type"]; !ok {
+			req.Header.Set("Content-Type", "application/xml")
 		}
 
-		client = &http.Client{
-			Transport: tr,
-			Timeout:   timeout,
-		}
-	} else {
-		client = &http.Client{
-			Timeout: timeout,
+		switch r.XML.(type) {
+		case map[string]string:
+			b, err := map2XML(r.XML.(map[string]string))
+			if err == nil {
+				req.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+			}
+		default:
+			b, err := xml.Marshal(r.JSON)
+			if err == nil {
+				req.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+			}
 		}
 	}
 
-	var resp *http.Response
-	var err error
+	// 创建客户端并发送请求
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 
-	switch method {
-	case "POST":
-		if formParams != nil {
-			request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			request.PostForm = formParams
+	if r.Proxy != "" {
+		proxy, err := url.Parse(r.Proxy)
+		if err == nil {
+			tr.Proxy = http.ProxyURL(proxy)
 		}
-		break
 	}
 
-	if jsonData != nil {
-		request.Header.Set("Content-Type", "application/json")
-		request.Body = ioutil.NopCloser(bytes.NewReader(jsonData))
+	client := &http.Client{
+		Timeout:   r.Timeout,
+		Transport: tr,
 	}
 
-	if wc.Debug {
-		logger.Debug(fmt.Sprintf("%s %s", method, url))
-	}
-
-	resp, err = client.Do(request)
+	resp, err := client.Do(req)
 
 	if err != nil {
 		return nil, err
@@ -158,140 +183,110 @@ func (wc *WebClient) doRequest(method, url string, queryParams map[string]string
 
 	defer resp.Body.Close()
 
-	if filename != "" {
-		if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
-			return nil, errors.New(fmt.Sprintf("A non-200 response status code was detected. (StatusCode: %d)", resp.StatusCode))
-		}
+	content, err := ioutil.ReadAll(resp.Body)
 
-		dirn := filepath.Dir(filename)
-
-		if !IsDir(dirn) {
-			os.MkdirAll(dirn, 0755)
-		}
-
-		out, err := os.Create(filename + ".tmp")
-		if err != nil {
-			return nil, err
-		}
-
-		// 此处不能使用 defer 方式关闭 out 资源，因为在 os.Rename 时资源句柄未释放造成重命名出错！
-		// defer out.Close()
-
-		var totalSize uint64
-
-		totalSize = 0
-		contentSize := resp.Header.Get("Content-Length")
-
-		if contentSize != "" {
-			intNum, _ := strconv.Atoi(contentSize)
-			totalSize = uint64(intNum)
-		}
-
-		counter := &ProgressBarCounter{ProgressBar: showProgressBar, TotalBytes: totalSize, SimpleBarStyle: true}
-
-		if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
-			out.Close()
-			return nil, err
-		}
-
-		out.Close()
-
-		if showProgressBar {
-			fmt.Print("\n")
-		}
-
-		if err = os.Rename(filename+".tmp", filename); err != nil {
-			return nil, err
-		}
-	} else {
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
-			return body, errors.New(fmt.Sprintf("A non-200 response status code was detected. (StatusCode: %d)", resp.StatusCode))
-		}
-
-		return body, nil
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, nil
-}
-
-func (wc *WebClient) Get(url string) ([]byte, error) {
-	return wc.doRequest("GET", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) GetWithQuery(url string, queryParams map[string]string) ([]byte, error) {
-	return wc.doRequest("GET", url, queryParams, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) Head(url string) ([]byte, error) {
-	return wc.doRequest("HEAD", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) Post(url string, formParams url2.Values) ([]byte, error) {
-	return wc.doRequest("POST", url, nil, formParams, nil, 0, "", false)
-}
-
-func (wc *WebClient) PostWithJSON(url string, jsonData []byte) ([]byte, error) {
-	return wc.doRequest("POST", url, nil, nil, jsonData, 0, "", false)
-}
-
-func (wc *WebClient) Put(url string) ([]byte, error) {
-	return wc.doRequest("PUT", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) Delete(url string) ([]byte, error) {
-	return wc.doRequest("DELETE", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) Options(url string) ([]byte, error) {
-	return wc.doRequest("OPTIONS", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) Patch(url string) ([]byte, error) {
-	return wc.doRequest("PATCH", url, nil, nil, nil, 0, "", false)
-}
-
-func (wc *WebClient) DownloadFile(url string, filename string, showProgress bool) error {
-	_, err := wc.doRequest("GET", url, nil, nil, nil, 0, filename, showProgress)
-
-	return err
-}
-
-// ProgressBarCounter counts the number of bytes written to it. It implements to the io.Writer interface
-// and we can pass this into io.TeeReader() which will report progress on each write cycle.
-type ProgressBarCounter struct {
-	LoadedBytes    uint64
-	TotalBytes     uint64
-	ProgressBar    bool
-	SimpleBarStyle bool
-}
-
-func (w *ProgressBarCounter) Write(p []byte) (int, error) {
-	n := len(p)
-	w.LoadedBytes += uint64(n)
-
-	if w.ProgressBar {
-		w.PrintProgress()
+	ret := &HttpResponse{
+		RequestURI:    resp.Request.RequestURI,
+		StatusCode:    resp.StatusCode,
+		Header:        resp.Header,
+		ContentType:   resp.Header.Get("Content-Type"),
+		ContentLength: resp.ContentLength,
+		Body:          content,
 	}
 
-	return n, nil
+	// 检查非 200 响应状态
+	if !r.AllowNon200Response && !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
+		return ret, errors.New(fmt.Sprintf("A non-200 response status code was detected. (StatusCode: %d)", resp.StatusCode))
+	}
+
+	return ret, nil
 }
 
-func (w ProgressBarCounter) PrintProgress() {
-	// Clear the line by using a character return to go back to the start and remove
-	// the remaining characters by filling it with spaces
-	// fmt.Printf("\r%s", strings.Repeat(" ", 35))
+func (h *HttpClient) Get(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("GET", uri, r)
+}
 
-	// Return again and print current status of download
-	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
-	if w.TotalBytes > 0 {
-		if w.SimpleBarStyle {
-			fmt.Printf("\r%.2f%%", float64(w.LoadedBytes)*100.00/float64(w.TotalBytes))
+func (h *HttpClient) Post(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("POST", uri, r)
+}
+
+func (h *HttpClient) Put(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("PUT", uri, r)
+}
+
+func (h *HttpClient) Delete(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("DELETE", uri, r)
+}
+
+func (h *HttpClient) Options(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("OPTIONS", uri, r)
+}
+
+func (h *HttpClient) Head(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("HEAD", uri, r)
+}
+
+func (h *HttpClient) Patch(uri string, r *HttpRequest) (*HttpResponse, error) {
+	return h.Request("PATCH", uri, r)
+}
+
+func map2XML(m map[string]string, opts ...interface{}) ([]byte, error) {
+	rootTag := "xml"
+	if len(opts) > 0 {
+		// the first opts is the root tag of xml struct
+		if v, ok := opts[0].(string); ok {
+			rootTag = v
+		}
+	}
+
+	d := mdata{xml.Name{Local: rootTag}, m}
+
+	bt, err := xml.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+
+	return bt, nil
+}
+
+type mdata struct {
+	XMLName xml.Name
+	data    map[string]string
+}
+
+// MarshalXML xml encode
+func (m mdata) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	if len(m.data) == 0 {
+		return nil
+	}
+
+	start.Name.Local = m.XMLName.Local
+
+	err := e.EncodeToken(start)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range m.data {
+		if strings.HasPrefix(v, "cdata:") {
+			v = strings.Replace(v, "cdata:", "", 1)
+			xs := struct {
+				XMLName xml.Name
+				Value   interface{} `xml:",cdata"`
+			}{xml.Name{Local: k}, v}
+			e.Encode(xs)
 		} else {
-			fmt.Printf("\rDownloading... %s of %s complete", humanize.Bytes(w.LoadedBytes), humanize.Bytes(w.TotalBytes))
+			xs := struct {
+				XMLName xml.Name
+				Value   interface{} `xml:",chardata"`
+			}{xml.Name{Local: k}, v}
+			e.Encode(xs)
 		}
-	} else {
-		fmt.Printf("\rDownloading... %s complete", humanize.Bytes(w.LoadedBytes))
 	}
+
+	return e.EncodeToken(start.End())
 }
